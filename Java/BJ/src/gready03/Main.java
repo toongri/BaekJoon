1c88e85dd0f817f0f410a8405a6d982a62d2d00e
Toong_laptop <jhkim03284@gmail.com>
2021-09-06 19:07:54 +0900

    자바 그리디 3번문제 구현

---
+++ Java/BJ/src/gready03/Main.java
@@ -0,0 +1,23 @@
+package gready03;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int cnt = 0;
+		
+		while(a >= b) {
+			cnt += a % b + 1;
+			a /= b;
+		}
+		
+		System.out.println(cnt);
+
+	}
+
+}
