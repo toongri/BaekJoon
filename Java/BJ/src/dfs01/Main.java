cef5de02e87244ebdb0c274c34dabc174c01eee1
Toong_laptop <jhkim03284@gmail.com>
2021-09-12 23:41:54 +0900

    dfs 1번문제 자바 풀이

--- Java/BJ/src/dfs01/Main.java
+++ Java/BJ/src/dfs01/Main.java
@@ -1,10 +1,18 @@
 package dfs01;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		
+		
 	}
 
 }
