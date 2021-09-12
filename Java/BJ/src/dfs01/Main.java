6584c35da0935b32554ffb5346be614e4fbf6315
Toong_laptop <jhkim03284@gmail.com>
2021-09-13 01:51:56 +0900

    dfs 001 java 구현

--- Java/BJ/src/dfs01/Main.java
+++ Java/BJ/src/dfs01/Main.java
@@ -3,16 +3,60 @@ package dfs01;
 import java.util.Scanner;
 
 public class Main {
+	static int[][] arrVst;
+	static int[][] arrGrp;
+	static int[][] arrMov = { { 1, 0, -1, 0 }, { 0, 1, 0, -1 } };
+	static final int lenMov = 4;
+	static int lenx;
+	static int leny;
+	static int cnt = 0;
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
-		int x = sc.nextInt();
-		int y = sc.nextInt();
-		
-		
-		
+
+		leny = sc.nextInt();
+		lenx = sc.nextInt();
+		sc.nextLine();
+
+		arrGrp = new int[leny][lenx];
+		arrVst = new int[leny][lenx];
+
+		for (int i = 0; i < leny; i++) {
+			String str = sc.nextLine();
+			for (int j = 0; j < lenx; j++) {
+				arrGrp[i][j] = str.charAt(j) - '0';
+			}
+		}
+
+		for (int i = 0; i < leny; i++) {
+			for (int j = 0; j < lenx; j++) {
+				if (arrGrp[i][j] == 0 && arrVst[i][j] == 0) {
+					cnt++;
+					dfs(i, j);
+				}
+
+			}
+		}
+
+		System.out.println(cnt);
+	}
+
+	public static void dfs(int y, int x) {
+		arrVst[y][x] = 1;
+		int px;
+		int py;
+
+		for (int i = 0; i < lenMov; i++) {
+			py = y + arrMov[0][i];
+			px = x + arrMov[1][i];
+			if (py >= 0 && py < leny && px >= 0 && px < lenx) {
+				if (arrGrp[py][px] == 0 && arrVst[py][px] == 0) {
+					dfs(py, px);
+				}
+			}
+
+		}
 	}
 
 }
